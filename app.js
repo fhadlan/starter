6fc8ba50c1b6e559c2008772da5c076ff7afe3d9
fhadlan <58333143+fhadlan@users.noreply.github.com>
2023-08-09 16:16:15 +0700

    Update app.js

--- app.js
+++ app.js
@@ -38,7 +38,9 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 // extra packages
-
+app.get("/",(req,res)=>{
+  res.send("jobs API")
+});
 // routes
 app.use("/api/v1/auth/", authRouter);
 app.use("/api/v1/jobs/", authenticateUser, jobsRouter);
