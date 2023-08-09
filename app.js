91ca1183b58fdef35bbca97a7b44398bb2585b61
fhadlan <58333143+fhadlan@users.noreply.github.com>
2023-08-09 16:22:21 +0700

    Update app.js

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.use("/api/v1/jobs/", authenticateUser, jobsRouter);
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 const start = async () => {
   try {
